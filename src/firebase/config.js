52df78be7ff6f2c42027409fc69a3f8c84339fb5
erictumuti <erictumuti114@gmail.com>
2020-12-30 13:14:48 +0300

    firebase and composables setup

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: "AIzaSyC6Yulq-0EHqW-2_HADkfkdRlHQrem0LwE",
+	authDomain: "vue-muso.firebaseapp.com",
+	projectId: "vue-muso",
+	storageBucket: "vue-muso.appspot.com",
+	messagingSenderId: "278527480581",
+	appId: "1:278527480581:web:047022065d2a20fdb19d6c"
+  };
+
+//   init firebase
+  firebase.initializeApp(firebaseConfig)
+
+//   init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+
+export { projectFirestore, projectAuth, timestamp }
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+	  <h3>Login Page</h3>
+	  <input type="email" v-model="email" placeholder="Email">
+	  <input type="password" v-model="password" placeholder="Password">
+	  <button>Log in</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
