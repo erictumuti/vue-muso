2aba260d86d0301bb255b267ef95aae800d0ddeb
erictumuti <erictumuti114@gmail.com>
2021-01-03 00:20:09 +0300

    list the created playlists in home view

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,7 @@
 		  <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
 		  <div class="links">
 			  <div v-if="user">
+				  <router-link :to="{ name: 'CreatePlaylist' }">Create playlist</router-link>
 				  <button @click="handleClick">logout</button>
 			  </div>
 			  <div v-else>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-	  <p>Home page</p>
+  <div v-if="documents">
+      <ListView :playlists="documents" />
+    </div>
   </div>
 </template>
 
 <script>
+import ListView from '@/components/ListView.vue'
+import getCollection from '@/composables/getCollection'
 export default {
-	name: 'Home'
+	name: 'Home',
+	components: { ListView },
+	setup() {
+		const { documents, error } = getCollection('playlists')
+
+		return { documents, error }
+	}
 }
 </script>
 
