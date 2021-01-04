aedba4bea6f78caade90ecaa2493fdc20dcdc8e8
erictumuti <erictumuti114@gmail.com>
2021-01-04 20:21:46 +0300

    add song to playlist details by the owner and also change firebase storage rules

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -11,12 +11,21 @@
 		  <button v-if="ownership" @click="handleDelete">Delete playlist</button>
 	  </div>
 	  <div class="song-list">
-		  <p>song list here</p>
+		  <div v-if="!playlist.songs.length">No songs have been added to this playlist</div>
+       <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+         <div class="details">
+           <h3>{{ song.title }}</h3>
+           <p>{{ song.artist }}</p>
+         </div>
+           <button v-if="ownership">delete</button>
+       </div>
+      <AddSong v-if="ownership" :playlist="playlist"/>
 	  </div>
   </div>
 </template>
 
 <script>
+import AddSong from '@/components/AddSong.vue'
 import useStorage from '@/composables/useStorage'
 import useDocument from '@/composables/useDocument'
 import getDocument from '@/composables/getDocument'
@@ -25,6 +34,7 @@ import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 export default {
   props: ['id'],
+  components: { AddSong },
   setup(props) {
     const { error, document: playlist } = getDocument('playlists', props.id)
     const { user } = getUser()
@@ -84,5 +94,13 @@ export default {
   }
   .description {
     text-align: left;
+  }
+  .single-song {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px dashed var(--secondary);
+    margin-bottom: 20px;
   }
 </style>
